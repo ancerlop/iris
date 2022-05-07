6748cb1a8a6395c09e237e4c90cead9b2d671b1b
ancerlop <99099925+ancerlop@users.noreply.github.com>
2022-05-07 02:04:42 +0200

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ data = data.replace(['Setosa', 'Versicolor' , 'Virginica'],[0, 1, 2])
 X = data.iloc[:, 0:-1] # Extracting the independent variables
 y = data.iloc[:, -1] # Extracting the target/dependent variable
 
-logreg = LogisticRegression() # Initializing the Logistic Regression model
+logreg = LogisticRegression(solver='lbfgs', max_iter=1000) # Initializing the Logistic Regression model
 logreg.fit(X, y) # Fitting the model
 
 # Function for classification based on inputs
